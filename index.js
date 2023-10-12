bb1dbb83e7c6cec8d6b0050e6ad0333ab3442e0c
ARINDOM DAS <145354131+Arindom-Das@users.noreply.github.com>
2023-10-12 20:03:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,24 +25,4 @@ document.getElementById("resetButton").addEventListener("click", function () {
     document.getElementById("number").textContent = "0";
 });
 
-/* 
- 
-EXPLAINATION :
 
-The code snippet you provided is written in JavaScript. 
-It appears to handle the functionality of rolling a dice and displaying the result on a webpage. 
-
-The code uses event listeners to trigger actions when certain buttons are clicked. 
-
-When the "rollButton" is clicked, it first checks if a roll is already in progress by checking the value of the "isLoading" variable. 
-If a roll is already in progress, it does nothing. Otherwise, it sets "isLoading" to true, displays a loading message, and hides the number display. 
-
-After a 2-second delay (simulated using the setTimeout function), it generates a random number between 1 and 6, 
-updates the number display with the generated number, and shows the number display again. 
-Finally, it sets "isLoading" to false.
-
-The "resetButton" event listener resets the state of the variables and displays to their initial values.
-
-Please note that this code snippet assumes the existence of HTML elements with specific IDs ("rollButton", "loading", "number", "resetButton") on the webpage. 
-
-*/
